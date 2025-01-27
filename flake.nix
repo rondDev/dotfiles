5fe2421f0bc1c0f26d658dc3ec407210d606cece
rondDev <contact@rond.cc>
2025-01-27 18:21:36 +0100

    feat: initial commit

---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  description = "rond's Home Manager and NixOS config";
+
+  inputs = {
+    # Nixpkgs
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+
+    # Home manager
+    home-manager.url = "github:nix-community/home-manager/release-24.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    # Lix
+    # lix-module = {}
+
+    # Hyprland
+    hyprland.url = "github:hyprwm/Hyprland";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+    ...
+  } @ inputs: let
+    inherit (self) outputs;
+  in {
+    # NixOS configuration entrypoint
+    # Available through 'nixos-rebuild --flake .#your-hostname'
+    nixosConfigurations = {
+      # FIXME replace with your hostname
+      nixos = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs outputs;};
+        # > Our main nixos configuration file <
+        modules = [./nixos/configuration.nix];
+      };
+    };
+
+    # Standalone home-manager configuration entrypoint
+    # Available through 'home-manager --flake .#your-username@your-hostname'
+    homeConfigurations = {
+      # FIXME replace with your username@hostname
+      "rond@nixos" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+        extraSpecialArgs = {inherit inputs outputs;};
+        # > Our main home-manager configuration file <
+        modules = [./home-manager/home.nix];
+      };
+    };
+  };
+}
---
+++ nixos/configuration.nix
@@ -0,0 +1,182 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ inputs, lib, config, pkgs, ... }:
+
+with lib;
+let
+hypr-mesa = inputs.hyprland.inputs.nixpkgs.legacyPackages.${pkgs.stdenv.hostPlatform.system};
+
+in {
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Oslo";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  # Enable the X11 windowing system.
+  #services.xserver.enable = true;
+
+  # Enable the Deepin Desktop Environment.
+  # services.xserver.displayManager.lightdm.enable = true;
+  # services.xserver.desktopManager.deepin.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  security.sudo.wheelNeedsPassword = false;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  services.qemuGuest.enable = true;
+  services.spice-vdagentd.enable = true;
+  services.xserver.videoDrivers = mkDefault [ "nvidia" ];
+  services.openssh.enable = true;
+  hardware = {
+    graphics = {
+      enable = true;
+      package = hypr-mesa.mesa.drivers;
+  #
+       enable32Bit = true;
+      package32 = hypr-mesa.pkgsi686Linux.mesa.drivers;
+       extraPackages = [ pkgs.vaapiVdpau ];
+    };
+    nvidia = {
+      # open = mkDefault true;
+      open = false;
+    #   powerManagement.enable = true;
+    #   modesetting.enable = true;
+      package =  config.boot.kernelPackages.nvidiaPackages.beta;
+    };
+  };
+  security.polkit.enable = true;
+
+  boot.kernelParams = [ "nvidia-drm.fbdev=1" ];
+  boot.kernelModules = [ "nvidia" ];
+  environment.systemPackages = with pkgs; [
+    libva
+  ];
+  environment.sessionVariables = {
+    LIBVA_DRIVER_NAME = "nvidia";
+    WLR_NO_HARDWARE_CURSORS = "1";
+
+    # May cause Firefox crashes
+    GBM_BACKEND = "nvidia-drm";
+
+
+    # If you face problems with Discord windows not displaying or screen
+    # sharing not working in Zoom, remove or comment this:
+    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+
+    # AQ_DRM_DEVICES = "/dev/dri/card1";
+  };
+
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.rond = {
+    isNormalUser = true;
+    description = "rond";
+    extraGroups = [ "networkmanager" "wheel" ];
+  };
+
+  # Enable automatic login for the user.
+  services.displayManager.autoLogin.enable = true;
+  services.displayManager.autoLogin.user = "rond";
+
+  # Install firefox.
+  programs.firefox.enable = true;
+  programs.hyprland = {
+    enable = true;
+    withUWSM = true;
+    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+    portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+  };
+	#  programs.uwsm = {
+	#    enable = true;
+	#    waylandCompositors = { 
+	#        hyprland = {
+	#   prettyName = "Hyprland";
+	#   comment = "hyprrrrrrr";
+	#   binPath = "/run/current-system/sw/bin/Hyprland";
+	# };
+	#      };
+	#  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+  nix.settings = {
+    experimental-features = [ "nix-command" "flakes" ];
+    substituters = ["https://hyprland.cachix.org"];
+    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+  };
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.11"; # Did you read the comment?
+  hardware.enableRedistributableFirmware = lib.mkDefault true;
+}

1c610d9445b2ba10c7ee3259488bb117e835c732
rondDev <contact@rond.cc>
2025-05-11 01:11:00 +0200

    fix: add ~ to cd call

--- local/node-exporter-ubuntu.rb
+++ local/node-exporter-ubuntu.rb
@@ -1,6 +1,6 @@
 `sudo useradd --no-create-home --shell /bin/false node_exporter`
 
-`cd`
+`cd ~`
 `wget https://github.com/prometheus/node_exporter/releases/download/v1.9.1/node_exporter-1.9.1.linux-amd64.tar.gz`
 
 `tar xvf node_exporter-1.9.1.linux-amd64.tar.gz`

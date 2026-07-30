ffcce4a0f1739c387156662b96eca19485a8c4f2
rondDev <contact@rond.cc>
2026-07-30 23:41:26 +0200

    feat: add fish

---
+++ config/fish/conf.d/fish_frozen_theme.fish
@@ -0,0 +1,47 @@
+# This file was created by fish when upgrading to version 4.3, to migrate
+# theme variables from universal to global scope.
+# Don't edit this file, as it will be written by the web-config tool (`fish_config`).
+# To customize your theme, delete this file and see
+#     help interactive#syntax-highlighting
+# or
+#     man fish-interactive | less +/^SYNTAX.HIGHLIGHTING
+# for appropriate commands to add to ~/.config/fish/config.fish instead.
+# See also the release notes for fish 4.3.0 (run `help relnotes`).
+
+set --global fish_color_autosuggestion 7081d0
+set --global fish_color_cancel f16c75 --reverse
+set --global fish_color_command 37f499
+set --global fish_color_comment 7081d0
+set --global fish_color_cwd a48cf2
+set --global fish_color_cwd_root f265b5
+set --global fish_color_end a48cf2
+set --global fish_color_error f16c75
+set --global fish_color_escape a48cf2
+set --global fish_color_history_current --bold
+set --global fish_color_host 04d1f9
+set --global fish_color_host_remote 04d1f9
+set --global fish_color_keyword a48cf2
+set --global fish_color_normal ebfafa
+set --global fish_color_operator a48cf2
+set --global fish_color_option f7c67f
+set --global fish_color_param 04d1f9
+set --global fish_color_quote f1fc79
+set --global fish_color_redirection ebfafa
+set --global fish_color_search_match --background=76639e
+set --global fish_color_selection --background=76639e
+set --global fish_color_status f16c75
+set --global fish_color_user 37f499
+set --global fish_color_valid_path --underline
+set --global fish_pager_color_background
+set --global fish_pager_color_completion ebfafa
+set --global fish_pager_color_description 7081d0
+set --global fish_pager_color_prefix 04d1f9
+set --global fish_pager_color_progress 7081d0
+set --global fish_pager_color_secondary_background
+set --global fish_pager_color_secondary_completion
+set --global fish_pager_color_secondary_description
+set --global fish_pager_color_secondary_prefix
+set --global fish_pager_color_selected_background --background=76639e
+set --global fish_pager_color_selected_completion
+set --global fish_pager_color_selected_description
+set --global fish_pager_color_selected_prefix

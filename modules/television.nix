3ee68ccc4463a36318e5778b664c3b8815539a8a
rondDev <contact@rond.cc>
2026-07-30 23:45:26 +0200

    feat: add television

---
+++ modules/television.nix
@@ -0,0 +1,9 @@
+{
+  programs.television = {
+    enable = true;
+    enableBashIntegration = true;
+    enableFishIntegration = true;
+    enableNushellIntegration = true;
+    enableZshIntegration = true;
+  };
+}
